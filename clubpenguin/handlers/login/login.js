667c449a3cc3f3e3a46e65f33e311eaf60623257
Adam <56496581+Adamb2019@users.noreply.github.com>
2020-05-31 17:40:12 +0100

    Add files via upload

--- clubpenguin/handlers/login/login.js
+++ clubpenguin/handlers/login/login.js
@@ -4,14 +4,14 @@ const database = require('../../database/database.js')
 const bcrypt = require('bcrypt')
 const penguin = require('../../penguin.js')
 const worlds = require('../../../connections/worlds.json')
-const errors = require('../../../connections/errors.json')
+const errors = require('../../errors.js')
 
 const server = net.createServer(function(connection) {
     let client = new penguin(connection)
-    console.log('Client connected to login server')
+    console.log('Penguin connected to login server')
 
     connection.on('end', function() {
-        console.log('Client disconnected from login server')
+        console.log('Penguin disconnected from login server')
     })
 
     connection.on('data', function(data) {
@@ -29,55 +29,54 @@ const server = net.createServer(function(connection) {
                 }
             }
         }
-
-        function policy(data) {
-            if(data === '<policy-file-request/>') {
-                console.log(`Recieved ${data}`)
-                return true
-            }
-        }
-
-        function verChk(data) {
-            if(data === "<msg t='sys'><body action='verChk' r='0'><ver v='153' /></body></msg>") {
-                console.log(`Recieved ${data}`)
-                return true
-            }
-        }
-
-        function rndK(data) {
-            if(data === "<msg t='sys'><body action='rndK' r='-1'></body></msg>") {
-                console.log(`Recieved ${data}`)
-                return true
-            }
-        }
-
-        function login(data) {
-            parseString(data, function (err, result) {
-                if(result) {
-                    let username = result.msg.body[0].login[0].nick[0].toLowerCase()
-                    let password = result.msg.body[0].login[0].pword[0]
-
-                    database.query(`SELECT * FROM penguins WHERE username = '${username}'`, async function(err, results) {
-                        if(results.length < 1) {
-                            client.send_error(errors.USERNAME_NOT_FOUND)
-                        } else {
-                            let pass = results[0].Password
-                            let compare = await bcrypt.compare(password, pass)
-                            if(compare === false) {
-                                client.send_error(errors.INCORRECT_PASSWORD)
-                                console.log(client.send_xt())
-                            } else {
-                                client.send_xt('l', -1)
-                            }
-                        }
-                    })
-                }
-            })
-        }
     })
-});
+})
 
 server.listen(worlds.login.port, function() {
     console.log('login server listening on port 6112')
 });
+
+function policy(data) {
+    if(data === '<policy-file-request/>') {
+        console.log(`Recieved ${data}`)
+        return true
+    }
+}
+
+function verChk(data) {
+    if(data === "<msg t='sys'><body action='verChk' r='0'><ver v='153' /></body></msg>") {
+        console.log(`Recieved ${data}`)
+        return true
+    }
+}
+
+function rndK(data) {
+    if(data === "<msg t='sys'><body action='rndK' r='-1'></body></msg>") {
+        console.log(`Recieved ${data}`)
+        return true
+    }
+}
+
+function login(data) {
+    parseString(data, function (err, result) {
+        if(result) {
+            let username = result.msg.body[0].login[0].nick[0].toLowerCase()
+            let password = result.msg.body[0].login[0].pword[0]
+
+            database.query(`SELECT * FROM penguins WHERE username = '${username}'`, async function(err, results) {
+                if(results.length < 1) {
+                    client.send_error(USERNAME_NOT_FOUND)
+                } else {
+                    let pass = results[0].Password
+                    let compare = await bcrypt.compare(password, pass)
+                    if(compare === false) {
+                        client.send_error(INCORRECT_PASSWORD)
+                    } else {
+                        client.send_xt('l', -1)
+                    }
+                }
+            })
+        }
+    })
+}
 
--- clubpenguin/handlers/login/world.js
+++ clubpenguin/handlers/login/world.js
@@ -1,15 +1,16 @@
 const net = require('net')
 const penguin = require('../../penguin.js')
+const login = require('./login.js')
 const worlds = require('../../../connections/worlds.json')
 
 // cant connect to a world server needs fixing
 
 const server = net.createServer(function(connection) {
     let client = new penguin(connection)
-    console.log('Client connected to world server')
+    console.log('Penguin connected to world server')
 
     connection.on('end', function() {
-        console.log('Client disconnected from world server')
+        console.log('Penguin disconnected from world server')
     })
 
     connection.on('data', function(data) {
