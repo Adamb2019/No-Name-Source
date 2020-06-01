f1d17b6acff58ae3089c868a859d355b1785b9d4
Adam <56496581+Adamb2019@users.noreply.github.com>
2020-06-01 02:10:25 +0100

    added playerString
    added playerString

--- clubpenguin/penguin.js
+++ clubpenguin/penguin.js
@@ -13,13 +13,36 @@ class penguin {
       }
   }
 
-  send_xt() {
-    const args = Array.prototype.join.call(arguments, '%')
-    this.socket.write('%xt%' + args + '%' + '\0');
+  send_xt(...args) {
+    let packet = args.join('%')
+    this.socket.write('%xt%' + packet + '%' + '\0')
   }
 
   send_error(error) {
-    this.socket.write('%xt%e%-1%' + error + '%' + '\0');
+    this.socket.write('%xt%e%-1%' + error + '%' + '\0')
+  }
+
+  playerString(result) {
+    let playerArray = [ 
+    this.id = result.ID,
+    this.username = result.Username,
+    45,
+    this.color = result.Color,
+    this.head = result.Head,
+    this.face = result.Face,
+    this.neck = result.Neck,
+    this.body = result.Body,
+    this.hand = result.Hand,
+    this.feet = result.Feet,
+    this.flag = result.Flag,
+    this.photo = result.photo,
+    this.x = 0,
+    this.y = 0,
+    this.frame = 1,
+    1
+    // this.rank = rank
+    ]
+    return playerArray.join('|')
   }
 }
 
