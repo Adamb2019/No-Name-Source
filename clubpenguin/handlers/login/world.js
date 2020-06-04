6d6a8ce33579efa34ea486128b5062e66819807c
Adam <56496581+Adamb2019@users.noreply.github.com>
2020-06-04 02:34:16 +0100

    world is now working
    world is now working

--- clubpenguin/handlers/login/world.js
+++ clubpenguin/handlers/login/world.js
@@ -1,6 +1,8 @@
 const net = require('net')
 const parseString = require('xml2js').parseString
 const penguin = require('../../penguin.js')
+const handleJoinRoom = require('../world/joinRoom.js')
+const handleJoinServer = require('../world/joinServer.js')
 const database = require('../../database/database.js')
 const errors = require('../../errors.js')
 const worlds = require('../../../connections/worlds.json')
@@ -9,12 +11,18 @@ let penguinsOnline = []
 
 const server = net.createServer(function(connection) {
     let client = new penguin(connection)
+    let joinRoom = new handleJoinRoom()
+    let joinServer = new handleJoinServer()
     penguinsOnline.push(connection)
     console.log(`Penguin connected to the world server || Their are currently ${penguinsOnline.length} penguin's online`)
 
+    // if(worlds.world.moderator === true && !client.moderator) {
+    //     console.log('Penguin trying to connect to a moderator server destorying socket')
+    //     client.disconnect()
+    // }
+
     connection.on('end', function() {
         penguinsOnline.splice(penguinsOnline.indexOf(connection), 1)
-        console.log(penguinsOnline.length)
         console.log(`Penguin disconnected to the world server || Their are currently ${penguinsOnline.length} penguin's online`)
     })
 
@@ -26,14 +34,14 @@ const server = net.createServer(function(connection) {
             if(rndK(data1)) {
                 client.send_xml('<msg t="sys"><body action="rndK" r="-1"><k>nodeJS</k></body></msg>')
             } else {
-                login(data1, client)
+                login(data1, client, joinServer)
             }
         }
     })
 });
 
 server.listen(worlds.world.port, function() {
-    console.log('world server listening on port 9875')
+    console.log('[Server] World server listening on port 9875')
 });
 
 function verChk(data) {
@@ -48,27 +56,29 @@ function rndK(data) {
     }
 }
 
-function login(data, client) {
+function login(data, client, joinServer) {
     parseString(data, function (err, result) {
         if(result) {
             let username = result.msg.body[0].login[0].nick[0].toLowerCase()
+            let nickname = result.msg.body[0].login[0].nick[0]
             let password = result.msg.body[0].login[0].pword[0]
             let key = password.substr(32)
             database.query(`SELECT * FROM penguins WHERE username = '${username}'`, async function(err, results) {
-                let loginKey = results[0].LoginKey
-                if(results[0].LoginKey === "") {
-                    client.disconnect()
-                } else {
-                    if(key === loginKey) {
-                        client.send_xt('l')
-                        client.send_xt('js', -1, 0, 1)
-                        client.send_xt('gps', -1, '')
-                        client.send_xt('lp', -1, client.playerString(results[0]))
+                database.query(`SELECT * FROM inventory WHERE username = '${username}'`, function(err, results1) {
+                    let loginKey = results[0].LoginKey
+
+                    if(results[0].LoginKey === "") {
+                        client.disconnect()
                     } else {
-                        client.send_error(INCORRECT_PASSWORD)
+                        if(key === loginKey) {
+                            joinServer.joinServer(results[0], client, results1[0])
+                        } else {
+                            client.send_error(INCORRECT_PASSWORD)
                     }
                 }
+                })
             })
         }
+
     })
 }
