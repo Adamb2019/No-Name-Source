c690436d933b7a18e39f645bf74b8d74ebff8a42
Adam <56496581+Adamb2019@users.noreply.github.com>
2020-06-08 00:17:38 +0100

    updated for redemption (still working on it)
    updated for redemption (still working on it)

--- clubpenguin/database/clubPenguin.sql
+++ clubpenguin/database/clubPenguin.sql
@@ -4,22 +4,23 @@ USE `clubPenguin`;
 
 DROP TABLE IF EXISTS `inventory`;
 CREATE TABLE `inventory` (
+    `ID` int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT 'Number of tables',
     `PenguinID` int(10) unsigned NOT NULL COMMENT 'Penguin ID',
     `Username` text NOT NULL COMMENT 'Penguin Username',
     `ItemID` text NOT NULL COMMENT 'Clothing Item ID',
-    PRIMARY KEY (`PenguinID`)
+    PRIMARY KEY (`ID`)
 ) ENGINE=InnoDB AUTO_INCREMENT=0 DEFAULT CHARSET=latin1 COMMENT='inventory';
 
 DROP TABLE IF EXISTS `penguins`;
 CREATE TABLE `penguins` (
     `ID` int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT 'Penguin ID',
     `Username` text NOT NULL COMMENT 'Usernames',
-    `Nickname` text NOT NULL COMMENT 'Nickname',
+    `Nickname` text NOT NULL COMMENT 'Nicknames',
     `Password` text NOT NULL COMMENT 'Passwords',
     `Email` text NOT NULL COMMENT 'Emails',
     `Created` text NOT NULL DEFAULT NOW() COMMENT 'Date Created',
     `LoginKey` text NOT NULL COMMENT 'Login Key',
-    `Approved` text NOT NULL DEFAULT 0 COMMENT 'Username Approved',
+    `Approved` text NOT NULL DEFAULT 0 COMMENT 'Is Approved',
     `Active` text NOT NULL DEFAULT 0 COMMENT 'Is Activated',
     `SafeChat` text NOT NULL DEFAULT 0 COMMENT 'Is Safe Chatted',
     `Moderator` text NOT NULL DEFAULT 0 COMMENT 'Is Moderator',
@@ -37,3 +38,19 @@ CREATE TABLE `penguins` (
     `Flag` text NOT NULL DEFAULT 0 COMMENT 'Pin Item',
     PRIMARY KEY (`ID`)
 ) ENGINE=InnoDB AUTO_INCREMENT=0 DEFAULT CHARSET=latin1 COMMENT='penguins';
+
+DROP TABLE IF EXISTS `Code`;
+CREATE TABLE `Redemption_Code` (
+    `ID` int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT 'Number of tables',
+    `Code` text NOT NULL COMMENT 'Redemption Code',
+    `Awards` text NOT NULL DEFAULT 1 COMMENT 'Redemption Awards',
+    PRIMARY KEY (`ID`)
+) ENGINE=InnoDB AUTO_INCREMENT=0 DEFAULT CHARSET=latin1 COMMENT='Redemption Code';
+
+DROP TABLE IF EXISTS `Penguin_Redemption`;
+CREATE TABLE `Penguin_Redemption` (
+    `ID` int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT 'Number of tables',
+    `Username` text NOT NULL COMMENT 'Penguin Username',
+    `Code` text NOT NULL COMMENT 'Redemption Code',
+    PRIMARY KEY (`ID`)
+) ENGINE=InnoDB AUTO_INCREMENT=0 DEFAULT CHARSET=latin1 COMMENT='Penguin Redemption';
