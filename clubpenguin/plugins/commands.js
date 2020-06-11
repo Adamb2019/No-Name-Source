018a840d7205c9a8d0f1d603ecdae10bda6ade0f
Adam <56496581+Adamb2019@users.noreply.github.com>
2020-06-11 06:13:46 +0100

    added some commands
    added some commands

--- clubpenguin/plugins/commands.js
+++ clubpenguin/plugins/commands.js
@@ -1,3 +1,25 @@
-const handlePLayersOnline = require('../handlers/world/commands/pLayersOnline.js')
+const fs = require('fs')
+const addItem = require('../handlers/world/commands/addItem.js')
+const handlePlayersOnline = require('../handlers/world/commands/playersOnline.js')
 
-let pLayersOnline = new handlePLayersOnline()
+let playersOnline = new handlePlayersOnline()
+let addItemCommand = new addItem()
+
+let prefix = "!"
+
+class handleCommands {
+    online(data, client, online) { // players online
+        playersOnline.playersOnline(data, client, online)
+    }
+
+    addItem(data, client) {
+        if(!client.moderator === '1' && !client.username === 'Adam'
+        && !client.username === 'Adam1') {
+            return false
+        } else {
+            addItemCommand.item(data, client)
+        }
+    }
+}
+
+module.exports = handleCommands
