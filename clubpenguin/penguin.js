1d25b16c2ad4e670af7e3a768dd28eb6333af153
Adam <56496581+Adamb2019@users.noreply.github.com>
2020-06-11 02:10:42 +0100

    fixed astrio barrier also fixed items
    fixed astrio barrier also fixed items

--- clubpenguin/penguin.js
+++ clubpenguin/penguin.js
@@ -2,299 +2,269 @@ const database = require('./database/database.js')
 const worlds = require('../connections/worlds.json')
 const rooms = require('./handlers/crumbs/rooms.json')
 const items = require('./handlers/crumbs/items.json')
+const Room = require('./room.js')
+const rank = require('./plugins/rank.js')
 
-let clients = []
+let penguins = {}
+let roomSystem = new Room()
+let ranking = new rank()
 
 class penguin {
-    constructor(socket) {
-      this.socket = socket
+  constructor(socket) {
+    this.socket = socket
+  }
+
+  disconnect() {
+    console.log('got destroyed')
+    this.socket.destroy()
+  }
+
+  send_xml(data) {
+    if(this.socket)  {
+      this.socket.write(data + '\0')
     }
+  }  
 
-    disconnect() {
-      console.log('got destroyed')
-      this.socket.destroy()
-      clients.splice(clients.indexOf(this.socket), 1)
-    }
+  send_xt(...args) {
+    let packet = args.join('%')
+    console.log(`[Info] OUTGOING XT: ${args}`)
+    this.socket.write('%xt%' + packet + '%' + '\0')
+  }
 
-    send_xml(data) {
-      if(this.socket)  {
-        this.socket.write(data + '\0')
-      }
-    }  
+  send_error(error) {
+    console.log(`[Info] OUTGOING ERROR: ${error}`)
+    this.socket.write('%xt%e%-1%' + error + '%' + '\0')
+  }
 
-    send_xt(...args) {
-      let packet = args.join('%')
-      console.log(`OUTGOING XT: ${args}`)
-      this.socket.write('%xt%' + packet + '%' + '\0')
-    }
+  playerStuff(result) { // for anything else when ur not sending the playerString
+    this.id = result.ID
+    this.username = result.Username
+    this.created = result.Created // doesnt return pLayers age rn
+    this.loginKey = result.LoginKey
+    this.approved = result.Approved
+    this.active = result.Active
+    this.safeChat = result.SafeChat
+    this.moderator = result.Moderator
+    this.banned = result.Banned
+    this.permaBan = result.PermaBan
+    this.color = result.Color
+    this.coins = result.Coins
+    this.head = result.Head
+    this.face = result.Face
+    this.neck = result.Neck
+    this.body = result.Body
+    this.hand = result.Hand
+    this.feet = result.Feet
+    this.photo = result.Photo
+    this.flag = result.Flag
+    this.rank = result.Rank
+  }
 
-    send_error(error) {
-      console.log(`OUTGOING ERROR: ${error}`)
-      this.socket.write('%xt%e%-1%' + error + '%' + '\0')
-    }
+  playerString(result) { // for load player packet
+    let playerArray = [ 
+    this.id = result.ID,
+    this.username = result.Username,
+    45,
+    this.color = result.Color,
+    this.head = result.Head,
+    this.face = result.Face,
+    this.neck = result.Neck,
+    this.body = result.Body,
+    this.hand = result.Hand,
+    this.feet = result.Feet,
+    this.flag = result.Flag,
+    this.photo = result.Photo,
+    this.x = 0,
+    this.y = 0,
+    this.frame = 1,
+    1, // is member
+    this.rank = result.Rank * 146, // membership rank
+    this.coins = result.Coins,
+    this.age = 182
+    ]
+    return playerArray.join('|')
+  }
 
-    playerStuff(result) { // for anything else when ur not sending the playerString
-      this.id = result.ID
-      this.username = result.Username
-      this.created = result.Created // doesnt return pLayers age rn
-      this.loginKey = result.LoginKey
-      this.approved = result.Approved
-      this.active = result.Active
-      this.safeChat = result.SafeChat
-      this.moderator = result.Moderator
-      this.banned = result.Banned
-      this.permaBan = result.PermaBan
-      this.color = result.Color
-      this.coins = result.Coins
-      this.head = result.Head
-      this.face = result.Face
-      this.neck = result.Neck
-      this.body = result.Body
-      this.hand = result.Hand
-      this.feet = result.Feet
-      this.photo = result.Photo
-      this.flag = result.Flag
-      this.rank = result.Rank
-    }
+  joinServer(data, client) {
+    client.send_xt('l')
+    client.send_xt('js', -1, 0, 1, data.Moderator)
+    // client.send_xt('gps', -1, '') pLayer stamps but rn stamps arent added
+    client.send_xt('lp', -1, client.playerString(data), client.coins, 0, 1440, Math.floor(new Date() / 1000), client.age, 1000, 187, "", 7)
+    client.sendInventory(client)
+    ranking.rankUpdate(client) // rank system
+    roomSystem.joinRoomOnLogin(data, client)
+  }
 
-    playerString(result) { // for load player packet
-      let playerArray = [ 
-      this.id = result.ID,
-      this.username = result.Username,
-      45,
-      this.age = 100, // for now random WILL change soon to pengo age
-      this.moderator = result.Moderator,
-      this.coins = result.Coins,
-      this.color = result.Color,
-      this.head = result.Head,
-      this.face = result.Face,
-      this.neck = result.Neck,
-      this.body = result.Body,
-      this.hand = result.Hand,
-      this.feet = result.Feet,
-      this.flag = result.Flag,
-      this.photo = result.Photo,
-      this.x = 0,
-      this.y = 0,
-      this.frame = 1,
-      1,
-      this.rank = result.Rank
-      ]
-      return playerArray.join('|')
-    }
-
-    async joinServer(data, client) {
-      client.send_xt('l')
-      client.send_xt('js', -1, 0, 1, data.Moderator)
-      // client.send_xt('gps', -1, '') pLayer stamps but rn stamps arent added
-      client.send_xt('lp', -1, client.playerString(data), client.coins, 0, 1440, Math.floor(new Date() / 1000), client.age, 1000, 187, "", 7)
-      client.sendInventory(client)
-      client.joinRoomOnLogin(data, client)
-      clients.push(client.username)
-    }
-
-    joinRoomOnLogin(data, client) {
-      let roomIds = [100] // put ids of the rooms you want to join on spawn
-      let randomRooms = roomIds[Math.floor(Math.random() * roomIds.length)]
-      if(randomRooms) {
-        clients.push(client.username)
-        client.send_xt('jr', -1, randomRooms, client.playerString(data))
-      } else {
-        client.disconnect()
-      }
-    }
-
-    doesRoomExist(data, client) {
-      database.query(`SELECT * FROM penguins WHERE username = '${client.username}'`, function(err, results) {
-        let data1 = data.split('%')
-        let roomID = data1[5]
-  
-        for(let index in rooms) {
-          let checkIfExist = rooms[index][roomID]['Room_Id']
-          if(checkIfExist > 900) {
-            client.send_xt('jg', -1, roomID) 
-          } else {
-            if(checkIfExist) {
-              clients.push(client.username)
-              client.send_xt('jr', -1, roomID, client.playerString(results[0]))
-            } else {
-              client.send_xt('jr', -1, 100, client.playerString(results[0]))
-            }
-          }
-        }
-      })
-    }
-
-    getInventory(client) {
-      return new Promise(function(resolve, reject) {
-        let inventory = []
-        database.query(`SELECT * FROM inventory WHERE username = '${client.username}'`, function(err, result) {
-          Object.keys(result).forEach(function (item) {
-            let items = result[item].ItemID
-            inventory.push(items)
-          })
-          return resolve(inventory.join('%'))
+  getInventory(client) {
+    return new Promise(function(resolve, reject) {
+      let inventory = []
+      database.query(`SELECT * FROM inventory WHERE username = '${client.username}'`, function(err, result) {
+        Object.keys(result).forEach(function (item) {
+          let items = result[item].ItemID
+          inventory.push(items)
         })
+        return resolve(inventory.join('%'))
       })
-    }
+    })
+  }
 
-    sendInventory(client) {
-      this.getInventory(client).then(inventory => {
-        if(inventory) {
-          return client.send_xt('gi', -1, inventory)
-        }
-      })
-    }
+  sendInventory(client) {
+    this.getInventory(client).then(inventory => {
+      if(inventory) {
+        return client.send_xt('gi', -1, inventory)
+      }
+    })
+  }
 
-    addItem(data, client) {
-      let data1 = data.split('%')
-      let itemID = data1[5]
-      let findItem = items.find(item => item.Paper_Item_Id == itemID)
-      let itemAmount = findItem.Cost
-      if(findItem) {
-        if(client.coins < itemAmount) {
-          client.send_error(NOT_ENOUGH_COINS)
-        } else {
-          client.removeCoins(client, itemAmount)
-          database.query(`INSERT INTO inventory (PenguinID, Username, ItemID) VALUES ('${client.id}', '${client.username}', '${itemID}')`)
-          return client.send_xt('ai', -1, itemID, client.coins)
-        }
+  addItem(data, client) {
+    let data1 = data.split('%')
+    let itemID = data1[5]
+    let findItem = items.find(item => item.Paper_Item_Id == itemID)
+    let itemAmount = findItem.Cost
+    if(findItem) {
+      if(client.coins < itemAmount) {
+        client.send_error(NOT_ENOUGH_COINS)
       } else {
-        return client.send_error(ITEM_DOES_NOT_EXIST)
+        client.removeCoins(client, itemAmount)
+        database.query(`INSERT INTO inventory (PenguinID, Username, ItemID) VALUES ('${client.id}', '${client.username}', '${itemID}')`)
+        return client.send_xt('ai', -1, itemID, client.coins)
+      }
+    } else {
+      return client.send_error(ITEM_DOES_NOT_EXIST)
+    }
+  }
+
+  addCoins(client, amount) {
+    let coinsAdd = amount
+    let newCoins = +client.coins + coinsAdd
+
+    database.query(`UPDATE penguins SET coins = '${newCoins}' WHERE username = '${client.username}'`)
+    client.coins = newCoins
+
+    return newCoins
+  } 
+
+  removeCoins(client, amount) {
+    let coinsRemove = amount
+    let newCoins = client.coins - coinsRemove
+
+    database.query(`UPDATE penguins SET coins = '${newCoins}' WHERE username = '${client.username}'`)
+    client.coins = newCoins
+
+    return newCoins
+  }
+
+  heartBeat(client) {
+    return client.send_xt('h', -1)
+  }
+
+  updateColor(data, client) {
+    let data1 = data.split('%')
+    let colorID = data1[5]
+
+    database.query(`UPDATE penguins SET color = '${colorID}' WHERE username = '${client.username}'`)
+    client.color = colorID
+    return client.send_xt('upc', -1, client.id, colorID)
+  }
+
+  updateHead(data, client) {
+    let data1 = data.split('%')
+    let headID = data1[5]
+
+    database.query(`UPDATE penguins SET Head = '${headID}' WHERE username = '${client.username}'`)
+    client.head = headID
+    return client.send_xt('uph', -1, client.id, headID)
+  }
+
+  
+  updateFace(data, client) {
+    let data1 = data.split('%')
+    let faceID = data1[5]
+
+    database.query(`UPDATE penguins SET Face = '${faceID}' WHERE username = '${client.username}'`)
+    client.face = faceID
+    return client.send_xt('upf', -1, client.id, faceID)
+  }
+
+  
+  updateNeck(data, client) {
+    let data1 = data.split('%')
+    let neckID = data1[5]
+
+    database.query(`UPDATE penguins SET Neck = '${neckID}' WHERE username = '${client.username}'`)
+    client.neck = neckID
+    return client.send_xt('upn', -1, client.id, neckID)
+  }
+
+  
+  updateBody(data, client) {
+    let data1 = data.split('%')
+    let bodyID = data1[5]
+
+    database.query(`UPDATE penguins SET Body = '${bodyID}' WHERE username = '${client.username}'`)
+    client.body = bodyID
+    return client.send_xt('upb', -1, client.id, bodyID)
+  }
+
+    
+  updateHand(data, client) {
+    let data1 = data.split('%')
+    let handID = data1[5]
+
+    database.query(`UPDATE penguins SET Hand = '${handID}' WHERE username = '${client.username}'`)
+    client.hand = handID
+    return client.send_xt('upa', -1, client.id, handID)
+  }
+
+  
+  updateFeet(data, client) {
+    let data1 = data.split('%')
+    let feetID = data1[5]
+
+    database.query(`UPDATE penguins SET Feet = '${feetID}' WHERE username = '${client.username}'`)
+    client.feet = feetID
+    return client.send_xt('upe', -1, client.id, feetID)
+  }
+
+
+  updateBackground(data, client) {
+    let data1 = data.split('%')
+    let backgroundID = data1[5]
+
+    database.query(`UPDATE penguins SET Photo = '${backgroundID}' WHERE username = '${client.username}'`)
+    client.photo = backgroundID
+    return client.send_xt('upp', -1, client.id, backgroundID)
+  }
+
+  
+  updatePin(data, client) {
+    let data1 = data.split('%')
+    let pinID = data1[5]
+
+    database.query(`UPDATE penguins SET Flag = '${pinID}' WHERE username = '${client.username}'`)
+    client.flag = pinID
+    return client.send_xt('upl', -1, client.id, pinID)
+  }
+  
+  gameOver(data, client) { // not working needs fixing
+    let data1 = data.split('%')
+    let score = data1[5]
+    console.log(score)
+    let addCoins = score / 10
+    console.log(addCoins)
+    let maxCoins = 15000
+    if(score >= maxCoins) {
+      client.send_error(800)
+      client.disconnect()
+    } else {
+      if(score <= 0) {
+        client.send_xt('zo', -1, client.coins, 0)
+      } else {
+        client.addCoins(client, addCoins)
+       return client.send_xt('zo', -1, client.coins, addCoins)
       }
     }
-
-    addCoins(client, amount) {
-      let coinsAdd = amount
-      let newCoins = +client.coins + coinsAdd
-
-      database.query(`UPDATE penguins SET coins = '${newCoins}' WHERE username = '${client.username}'`)
-      client.coins = newCoins
-
-      return newCoins
-    } 
-
-    removeCoins(client, amount) {
-      let coinsRemove = amount
-      let newCoins = client.coins - coinsRemove
-
-      database.query(`UPDATE penguins SET coins = '${newCoins}' WHERE username = '${client.username}'`)
-      client.coins = newCoins
-  
-      return newCoins
-    }
-
-    heartBeat(client) {
-      return client.send_xt('h', -1)
-    }
-  
-    updateColor(data, client) {
-      let data1 = data.split('%')
-      let colorID = data1[5]
-  
-      database.query(`UPDATE penguins SET color = '${colorID}' WHERE username = '${client.username}'`)
-      client.color = colorID
-      return client.send_xt('upc', -1, client.id, colorID)
-    }
-
-    updateHead(data, client) {
-      let data1 = data.split('%')
-      let headID = data1[5]
-  
-      database.query(`UPDATE penguins SET Head = '${headID}' WHERE username = '${client.username}'`)
-      client.head = headID
-      return client.send_xt('uph', -1, client.id, headID)
-    }
-
-    
-    updateFace(data, client) {
-      let data1 = data.split('%')
-      let faceID = data1[5]
-  
-      database.query(`UPDATE penguins SET Face = '${faceID}' WHERE username = '${client.username}'`)
-      client.face = faceID
-      return client.send_xt('upf', -1, client.id, faceID)
-    }
-
-    
-    updateNeck(data, client) {
-      let data1 = data.split('%')
-      let neckID = data1[5]
-  
-      database.query(`UPDATE penguins SET Neck = '${neckID}' WHERE username = '${client.username}'`)
-      client.neck = neckID
-      return client.send_xt('upn', -1, client.id, neckID)
-    }
-
-    
-    updateBody(data, client) {
-      let data1 = data.split('%')
-      let bodyID = data1[5]
-  
-      database.query(`UPDATE penguins SET Body = '${bodyID}' WHERE username = '${client.username}'`)
-      client.body = bodyID
-      return client.send_xt('upb', -1, client.id, bodyID)
-    }
-
-    
-    updateHand(data, client) {
-      let data1 = data.split('%')
-      let handID = data1[5]
-  
-      database.query(`UPDATE penguins SET Hand = '${handID}' WHERE username = '${client.username}'`)
-      client.hand = handID
-      return client.send_xt('upa', -1, client.id, handID)
-    }
-
-    
-    updateFeet(data, client) {
-      let data1 = data.split('%')
-      let feetID = data1[5]
-  
-      database.query(`UPDATE penguins SET Feet = '${feetID}' WHERE username = '${client.username}'`)
-      client.feet = feetID
-      return client.send_xt('upe', -1, client.id, feetID)
-    }
-
-
-    updateBackground(data, client) {
-      let data1 = data.split('%')
-      let backgroundID = data1[5]
-  
-      database.query(`UPDATE penguins SET Photo = '${backgroundID}' WHERE username = '${client.username}'`)
-      client.photo = backgroundID
-      return client.send_xt('upp', -1, client.id, backgroundID)
-    }
-
-    
-    updatePin(data, client) {
-      let data1 = data.split('%')
-      let pinID = data1[5]
-
-      database.query(`UPDATE penguins SET Flag = '${pinID}' WHERE username = '${client.username}'`)
-      client.flag = pinID
-      return client.send_xt('upl', -1, client.id, pinID)
-    }
-  
-    gameOver(data, client) { // not working needs fixing
-      let data1 = data.split('%')
-      let score = data1[5]
-      console.log(score)
-      let addCoins = score / 10
-      console.log(addCoins)
-      let maxCoins = 15000
-      if(score >= maxCoins) {
-        client.send_error(800)
-        client.disconnect()
-      } else {
-        if(score <= 0) {
-          client.send_xt('zo', -1, client.coins, 0)
-        } else {
-          client.addCoins(client, addCoins)
-         return client.send_xt('zo', -1, client.coins, addCoins)
-        }
-      }
-    }
+  }
 }
 
 module.exports = penguin
