102769d64f204403cbbf3f9c97a4ca82219b0dff
Adam <56496581+Adamb2019@users.noreply.github.com>
2020-06-12 08:54:00 +0100

    updated
    updated

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const fs = require('fs')
 const Path = require('Path')
 const login = require('./clubpenguin/handlers/login/login.js')
 const world = require('./clubpenguin/handlers/login/world.js')
-const database = require('./clubpenguin/database/database.js')
 const redemption = require('./clubpenguin/handlers/redemption/redemption.js')
 const databaseInfo = require('./connections/database.json')
 const worlds = require('./connections/worlds.json')
@@ -12,16 +11,12 @@ const igloo = require('./clubpenguin/handlers/crumbs/igloo.json')
 const iglooFloor = require('./clubpenguin/handlers/crumbs/iglooFloor.json')
 const furniture = require('./clubpenguin/handlers/crumbs/furniture.json')
 
-let cmdPath = Path.join(__dirname, '/clubpenguin/handlers/world/commands/')
 let pluginsPath = Path.join(__dirname, '/clubpenguin/plugins/')
-
-fs.readdir(cmdPath, function(err, results) {
-    console.log(`[Server] Loaded ${results.length} command's`)
-});
 
 fs.readdir(pluginsPath, function(err, results) {
     console.log(`[Server] Loaded ${results.length} plugin's`)
 });
+
 
 console.log(`[Server] An as2 Club Penguin emulator written in Node JS`)
 console.log(`[Server] Loaded ${Object.keys(items).length} Items`)
