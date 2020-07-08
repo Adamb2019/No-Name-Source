1082be41b087684742a5afabf4811abf1b940a43
Adam <56496581+Adamb2019@users.noreply.github.com>
2020-07-08 20:00:09 +0100

    Added full room system
    Added full room system

--- clubpenguin/database/database_manager.js
+++ clubpenguin/database/database_manager.js
@@ -2,10 +2,6 @@ const database = require('./database.js')
 
 class database_manager {
     getPenguinTable(data, columnName) {
-        if(data === '' ) {
-            return false
-        }
-
         return new Promise(async function(resolve, reject) {
             await database.query(`SELECT * FROM penguins WHERE ${columnName} = ?`, [data], function(err, results) {
                 return resolve(results)
@@ -21,22 +17,6 @@ class database_manager {
         data1 = what we are pulling it from so from username Adam
         data2 = WHERE statement so ADAM penguin NAME ETC
         */
-       if(columnName === '') {
-           return false
-       }
-
-       if(data === '' ) {
-            return false
-        }
-
-        if(data1 === '') {
-            return false
-        }
-
-        if(data2 === '') {
-            return false
-        }
-
         return new Promise(async function(resolve, reject) {
             await database.query(`UPDATE penguins SET ${columnName} = ? WHERE ${data1} = ?`, [data, data2], function(err, results) {
                 return resolve(results)
@@ -45,10 +25,6 @@ class database_manager {
     }
 
     getInventoryTable(data, columnName) {
-        if(data === '') {
-            return false
-        }
-
         return new Promise(async function(resolve, reject) {
             await database.query(`SELECT * FROM inventory WHERE ${columnName} = ?`, [data], function(err, results) {
                 return resolve(results)
@@ -64,34 +40,15 @@ class database_manager {
         data1 = what we are pulling it from so from username Adam
         data2 = WHERE statement so ADAM penguin NAME ETC
         */
-       if(columnName === '') {
-           return false
-       }
-
-       if(data === '' ) {
-           return false
-        }
-
-       if(data1 === '') {
-           return false
-        }
-
-        if(data2 === '') {
-            return false
-        }
-
         return new Promise(async function(resolve, reject) {
             await database.query(`UPDATE inventory SET ${columnName} = ? WHERE ${data1} = ?`, [data, data2], function(err, results) {
+                console.log('ipdated table')
                 return resolve(results)
             })
         })
     }
 
     getIPBansTable(data, columnName) {
-        if(data === '') {
-            return false
-        }
-
         return new Promise(async function(resolve, reject) {
             await database.query(`SELECT * FROM ip_bans WHERE ${columnName} = ?`, [data], function(err, results) {
                 return resolve(results)
@@ -107,22 +64,6 @@ class database_manager {
         data1 = what we are pulling it from so from username Adam
         data2 = WHERE statement so ADAM penguin NAME ETC
         */
-       if(columnName === '') {
-           return false
-       }
-
-       if(data === '' ) {
-           return false
-        }
-
-       if(data1 === '') {
-           return false
-        }
-
-        if(data2 === '') {
-            return false
-        }
-
         return new Promise(async function(resolve, reject) {
             await database.query(`UPDATE ip_bans SET ${columnName} = ? WHERE ${data1} = ?`, [data, data2], function(err, results) {
                 return resolve(results)
@@ -130,11 +71,7 @@ class database_manager {
         })
     }
 
-    getLoginTable(data, column) {
-        if(data === '') {
-            return false
-        }
-
+    getLoginTable(data, columnName) {
         return new Promise(async function(resolve, reject) {
             await database.query(`SELECT * FROM login WHERE ${columnName} = ?`, [data], function(err, results) {
                 return resolve(results)
@@ -150,22 +87,6 @@ class database_manager {
         data1 = what we are pulling it from so from username Adam
         data2 = WHERE statement so ADAM penguin NAME ETC
         */
-       if(columnName === '') {
-           return false
-       }
-
-       if(data === '' ) {
-           return false
-        }
-
-       if(data1 === '') {
-           return false
-        }
-
-        if(data2 === '') {
-            return false
-        }
-
         return new Promise(async function(resolve, reject) {
             await database.query(`UPDATE login SET ${columnName} = ? WHERE ${data1} = ?`, [data, data2], function(err, results) {
                 return resolve(results)
@@ -174,10 +95,6 @@ class database_manager {
     }
 
     getPenguinRedemptionTable(data, columnName) {
-        if(data === '') {
-            return false
-        }
-
         return new Promise(async function(resolve, reject) {
             await database.query(`SELECT * FROM penguin_redemption WHERE ${columnName} = ?`, [data], function(err, results) {
                 return resolve(results)
@@ -193,22 +110,6 @@ class database_manager {
         data1 = what we are pulling it from so from username Adam
         data2 = WHERE statement so ADAM penguin NAME ETC
         */
-       if(columnName === '') {
-           return false
-       }
-
-       if(data === '' ) {
-           return false
-        }
-
-       if(data1 === '') {
-           return false
-        }
-
-        if(data2 === '') {
-            return false
-        }
-
         return new Promise(async function(resolve, reject) {
             await database.query(`UPDATE penguin_redemption SET ${columnName} = ? WHERE ${data1} = ?`, [data, data2], function(err, results) {
                 return resolve(results)
@@ -216,11 +117,7 @@ class database_manager {
         })
     }
 
-    getRedemptionCodeTable(data, column) {
-        if(data === '') {
-            return false
-        }
-
+    getRedemptionCodeTable(data, columnName) {
         return new Promise(async function(resolve, reject) {
             await database.query(`SELECT * FROM redemption_code WHERE ${columnName} = ?`, [data], function(err, results) {
                 return resolve(results)
@@ -236,29 +133,58 @@ class database_manager {
         data1 = what we are pulling it from so from username Adam
         data2 = WHERE statement so ADAM penguin NAME ETC
         */
-       if(columnName === '') {
-           return false
-       }
-
-       if(data === '' ) {
-           return false
-        }
-
-       if(data1 === '') {
-           return false
-        }
-
-        if(data2 === '') {
-            return false
-        }
-
         return new Promise(async function(resolve, reject) {
             await database.query(`UPDATE redemption_code SET ${columnName} = ? WHERE ${data1} = ?`, [data, data2], function(err, results) {
                 return resolve(results)
             })
         })
+    }   
+
+    getIglooTable(data, columnName) {
+        return new Promise(async function(resolve, reject) {
+            await database.query(`SELECT * FROM Igloo WHERE ${columnName} = ?`, [data], function(err, results) {
+                return resolve(results)
+            })
+        })
     }
-        
+
+    updateIglooTable(data, columnName, data1, data2) {
+        /*
+        im dumb and will forget soooo
+        data = thing we are updating
+        columnName = what column we are updating
+        data1 = what we are pulling it from so from username Adam
+        data2 = WHERE statement so ADAM penguin NAME ETC
+        */
+        return new Promise(async function(resolve, reject) {
+            await database.query(`UPDATE Igloo SET ${columnName} = ? WHERE ${data1} = ?`, [data, data2], function(err, results) {
+                return resolve(results)
+            })
+        })
+    }
+    
+    getRoomTable(data, columnName) {
+        return new Promise(async function(resolve, reject) {
+            await database.query(`SELECT * FROM Rooms WHERE ${columnName} = ?`, [data], function(err, results) {
+                return resolve(results)
+            })
+        })
+    }
+
+    updateRoomTable(data, columnName, data1, data2) {
+        /*
+        im dumb and will forget soooo
+        data = thing we are updating
+        columnName = what column we are updating
+        data1 = what we are pulling it from so from username Adam
+        data2 = WHERE statement so ADAM penguin NAME ETC
+        */
+       return new Promise(async function(resolve, reject) {
+           await database.query(`UPDATE Rooms SET ${columnName} = ? WHERE ${data1} = ?`, [data, data2], function(err, results) {
+               return resolve(results)
+           })
+       })
+    }
 }
 
 
