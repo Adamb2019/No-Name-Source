9c23fcf904e0675b2cf90263f6a9ea3dbcb7f49f
Adam <56496581+Adamb2019@users.noreply.github.com>
2020-09-02 05:38:17 +0100

    made the code cleaner
    made the code cleaner

--- clubpenguin/handlers/login/login.js
+++ clubpenguin/handlers/login/login.js
@@ -47,18 +47,21 @@ server.listen(worlds.login.port, function() {
 
 function policy(data) {
     if(data === '<policy-file-request/>') {
+        console.log(`[Info] INCOMING XML: ${data}`)
         return true
     }
 }
 
 function verChk(data) {
     if(data === "<msg t='sys'><body action='verChk' r='0'><ver v='153' /></body></msg>") {
+        console.log(`[Info] INCOMING XML: ${data}`)
         return true
     }
 }
 
 function rndK(data) {
     if(data === "<msg t='sys'><body action='rndK' r='-1'></body></msg>") {
+        console.log(`[Info] INCOMING XML: ${data}`)
         return true
     }
 }
@@ -66,6 +69,7 @@ function rndK(data) {
 function login(data, client) {
     parseString(data, function (err, result) {
         if(result) {
+            console.log(`[Info] INCOMING XML: ${data}`)
             let generate = new crypto()
             let username = result.msg.body[0].login[0].nick[0].toLowerCase()
             let password = result.msg.body[0].login[0].pword[0]
@@ -73,46 +77,46 @@ function login(data, client) {
             getDatabase.getPenguinTable(username, 'username').then(async exists => {
                 if(exists.length < 1) {
                     console.log(`${username} failed to login as username doesnt exist`)
-                    client.send_error(USERNAME_NOT_FOUND)
-                } else {
-                    let pass = exists[0].Password
-                    let id = exists[0].ID
-                    let compare = await bcrypt.compare(password, pass)
-                    if(compare === false) {
-                        console.log(`${username} failed to login as password doesnt match`)
-                        client.send_error(INCORRECT_PASSWORD)
-                    } else {
-                        if(exists[0].Active === '0') {
-                            client.send_error(ACCOUNT_NOT_ACTIVATED)
-                        } else {
-                            if(exists[0].PermaBan === '1') {
-                                client.send_error(BAN_FOREVER)
-                            } else {
-                                fetch('https://api.ipify.org?format=json')
-                                .then(res => res.json())
-                                .then(json => {
-                                    getDatabase.getIPBansTable(json.ip, 'IPAddress').then(exists1 => {
-                                        if(exists1.length >= 1) {
-                                            client.disconnect()
-                                        } else {
-                                            getDatabase.updatePenguinTable(randomKey, 'LoginKey', 'username', username).then(exists2 => {
-                                                if(exists2) {
-                                                    getDatabase.getPenguinTable(username, 'username').then(exists3 => {
-                                                        if(exists3.length >= 1) {
-                                                            let loginKey = exists3[0].LoginKey
-                                                            database.query(`INSERT INTO Login (PenguinID, Username, IPAddress) VALUES ('${exists3[0].ID}', '${exists3[0].Username}', '${json.ip}')`)
-                                                            client.send_xt('l', -1, id, loginKey, '', '100,5')
-                                                        }
-                                                    })
-                                                }
-                                            })
-                                        }
-                                    })
+                    return client.send_error(USERNAME_NOT_FOUND)
+                }
+
+                let pass = exists[0].Password
+                let id = exists[0].ID
+                let compare = await bcrypt.compare(password, pass)
+                if(compare === false) {
+                    console.log(`${username} failed to login as password doesnt match`)
+                    return client.send_error(INCORRECT_PASSWORD)
+                }
+
+                if(exists[0].Active === '0') {
+                    return client.send_error(ACCOUNT_NOT_ACTIVATED)
+                }
+
+                if(exists[0].PermaBan === '1') {
+                    return client.send_error(BAN_FOREVER)
+                }
+
+                fetch('https://api.ipify.org?format=json')
+                .then(res => res.json())
+                .then(json => {
+                    getDatabase.getIPBansTable(json.ip, 'IPAddress').then(exists1 => {
+                        if(exists1.length >= 1) {
+                            return client.disconnect()
+                        }
+
+                        getDatabase.updatePenguinTable(randomKey, 'LoginKey', 'username', username).then(exists2 => {
+                            if(exists2) {
+                                getDatabase.getPenguinTable(username, 'username').then(exists3 => {
+                                    if(exists3.length >= 1) {
+                                        let loginKey = exists3[0].LoginKey
+                                        database.query(`INSERT INTO Login (PenguinID, Username, IPAddress) VALUES ('${exists3[0].ID}', '${exists3[0].Username}', '${json.ip}')`)
+                                        return client.send_xt('l', -1, id, loginKey, '', '100,5')
+                                    }
                                 })
                             }
-                        }
-                    }
-                }
+                        })
+                    })
+                })
             })
         }
     })
